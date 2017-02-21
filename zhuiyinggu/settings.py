8c2edc5a5709e78dc5103f309fb2af1f9246c50b
changdapeng <dapeng_chang@163.com>
2017-02-21 16:02:23 +0800

    1、新增 blog 应用。

--- zhuiyinggu/settings.py
+++ zhuiyinggu/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'blog.apps.BlogConfig',
     'rest_framework',
     'guardian',
     'rest_framework.authtoken',
