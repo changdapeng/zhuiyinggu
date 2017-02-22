6fa3c35806bf158b8ef2ce64357263d6d34c7f08
changdapeng <dapeng_chang@163.com>
2017-02-22 15:30:14 +0800

    1、为 blog应用中的Blog模型增加content字段，并修改author字段类型为外键于User

--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,18 @@ blog/models.py
 自定义Blog模型
 """
 from django.db import models
+from django.conf import settings
 
 
 
 # blog 模型
 # ---------
 class Blog(models.Model):
-	name = models.CharField(max_length=200)
-	author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='blog_author')
+    content = models.TextField()
 
 
+
+    def __str__(self):
+        return self.title
