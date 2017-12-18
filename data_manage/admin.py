c39e0aedf4c05205e2129db8319bd6122af114c5
changdapeng <dapeng_chang@163.com>
2017-12-18 17:20:14 +0800

    1、在data_manage应用的model模型中删除Data模型 2、在admin中取消对Data模型的注册

--- data_manage/admin.py
+++ data_manage/admin.py
@@ -4,25 +4,8 @@ data_manage/admin.py
 
 from django.contrib import admin
 
-from data_manage.models import Data, Book, Music, Video, Movie, Game
+from data_manage.models import Book, Music, Video, Movie, Game
 
-
-
-# Data Admin管理界面管理器
-# ------------------------
-class DataAdmin(admin.ModelAdmin):
-
-    list_display = ('name', 'description', 'step', 'data_type', 'size', 'creator', 'create_date', 'address_type', 'address', 'path',)
-    list_filter = ('data_type', 'step', 'creator', 'create_date',)
-    fieldsets = (
-        ('Data info', {'fields': ('name', 'description', 'step', 'data_type', 'size', 'address_type', 'address', 'path',)}),
-        ('create info', {'fields': ('creator', 'create_date',)}),
-    )
-    search_fields = ('name', 'path', 'step', 'data_type')
-    ordering = ('name', 'size',)
-
-
-admin.site.register(Data, DataAdmin)
 
 
 
--- data_manage/models.py
+++ data_manage/models.py
@@ -5,43 +5,6 @@ Data: 资料管理 模型
 """
 from django.db import models
 
-DATA_STEP = (
-('setp1','待下载'),
-('setp2', '已下载'),
-('step3', '审核成功'),
-)
-
-DATA_TYPE = (
-('type1','电影'),
-('type2','电视剧'),
-('type3','游戏'),
-('type4','音乐'),
-('type5','IT技术视频'),
-('type6','IT技术文档'),
-('type7','IT行业视频'),
-('type8','书籍'),
-('type9','视频资料'),
-)
-
-
-# 资料管理 模型
-# ----------------
-class Data(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(blank=True, max_length=200)
-    step = models.CharField(choices=DATA_STEP, blank=True, max_length=50)
-    data_type = models.CharField(choices= DATA_TYPE, blank=True, max_length=50)
-    size = models.FloatField(blank=True, null=True)
-    creator = models.CharField(max_length=200)
-    create_date = models.DateField(blank=True, null=True)
-    address = models.CharField(blank=True, max_length=200)
-    address_type = models.CharField(choices=(('bd','百度云'), ('bt', 'bt种子')), blank=True, max_length=50)
-    path = models.CharField(blank=True, max_length=200)
-    
-        
-    def __str__(self):
-        
-        return self.name
 
 
 
